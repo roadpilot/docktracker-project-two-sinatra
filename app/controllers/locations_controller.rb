bf268d1e6be9ec90b9d2d407cc421f9ca91b2193
mdacht777 <mdacht777@gmail.com>
2020-09-13 21:23:59 -0600

    working on CRUD

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -14,13 +14,35 @@ class LocationsController < ApplicationController
     erb :"/locations/new.html"
   end
 
+  # POST: /locations/new
+  post "/locations/new" do
+    if params[:business_name] != "" and params[:address] != ""
+      @location=Location.create(user_id: session[:user_id], business_name: params[:business_name], address: params[:address])
+      if params[:comments] != "" or params[:friendly53]=="1"
+        Comment.create(location_id: @location.id, user_id: session[:user_id], comment: params[:comments], friendly53: params[:friendly53])
+      end
+    else
+    end
+    # binding.pry
+    @result = Location.all
+    erb :"/locations/index.html"
+  end
+
   # POST: /locations
   post "/locations" do
-    redirect "/locations"
+    @search = params[:search]
+    @result = []
+    if @search != ""
+      @result = Location.where("business_name LIKE (?) or address LIKE (?)", "#{params[:search]}%","#{params[:search]}%")
+    end
+    erb :"/locations/index.html"
   end
 
   # GET: /locations/5
   get "/locations/:id" do
+    @location = Location.find(params[:id])
+    @comments = Comment.find_by(location_id: @location.id)
+    binding.pry
     erb :"/locations/show.html"
   end
 
