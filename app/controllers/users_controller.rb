53d9d383c9785cf8f6c872c410705750371fda2c
mdacht777 <mdacht777@gmail.com>
2020-09-19 14:57:13 -0600

    Users controller cleanup and commenting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,11 @@
 class UsersController < ApplicationController
 
+  # LOG IN THE USER
   post "/" do
     @user = User.find_by(handle: params[:handle])
     # binding.pry
     if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
-        # flash[:message] = "Welcome, #{@user.username}"
-        # # redirect "/tweets/#{@user.id}"
-        # # binding.pry
-        # redirect '/tweets'
-        # logged_in?.to_s
     else
         # show an error message
         flash[:error] = "Either your handle or your password was wrong. Try again!"
@@ -17,16 +13,13 @@ class UsersController < ApplicationController
     redirect '/'
   end
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
-  end
-
+  # CREATE NEW USER FORM
   # GET: /users/new
   get "/users/new" do
     erb :"/users/new.html"
   end
 
+  # CREATE NEW USER AND SEND THEM TO SEARCH FORM
   # POST: /users/new
   post "/users/new" do
   # if params[:handle].empty? || params[:email].empty? || params[:password].empty?
@@ -41,35 +34,40 @@ class UsersController < ApplicationController
     end
   end
 
-
-  
-  # POST: /users
-  post "/users" do
-    redirect "/users"
-  end
-
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
-
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
-
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
-
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
-
+  # LOG OFF THE USER
   get "/logoff" do
     session.clear
     redirect "/"
   end
+
+  # POST: /users  RESERVED FOR FUTURE USE
+  # post "/users" do
+  #   redirect "/users"
+  # end
+
+  # GET: /users/5  RESERVED FOR FUTURE USE
+  # get "/users/:id" do
+  #   erb :"/users/show.html"
+  # end
+
+  # GET: /users/5/edit  RESERVED FOR FUTURE USE
+  # get "/users/:id/edit" do
+  #   erb :"/users/edit.html"
+  # end
+
+  # PATCH: /users/5  RESERVED FOR FUTURE USE
+  # patch "/users/:id" do
+  #   redirect "/users/:id"
+  # end
+
+  # DELETE: /users/5/delete  RESERVED FOR FUTURE USE
+  # delete "/users/:id/delete" do
+  #   redirect "/users"
+  # end
+
+  # GET: /users  RESERVED FOR FUTURE USE
+  # get "/users" do
+  #   erb :"/users/index.html"
+  # end
+
 end
